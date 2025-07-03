978ab9e52809a3aeff109a1fb3f294dc1f2f4286
daekwon <pranludi@gmail.com>
2025-07-03 20:48:33 +0900

    first

---
+++ src/main/java/com/example/shop/ShopApplication.java
@@ -0,0 +1,14 @@
+package com.example.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class ShopApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(ShopApplication.class, args);
+    }
+
+}
